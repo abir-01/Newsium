00057345bafbcb8ed4ac89411bd67619fbcc13ac
abir-01 <abir.rakshit@gmail.com>
2023-03-17 22:20:57 +0530

    added hash router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
 import * as te from 'tw-elements';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>
 );
